110ee702048469d02e75aec03c6fa4b34d6294ea
Yuntong Zhou <joytzphysics@uaf-10.t2.ucsd.edu>
2022-08-21 15:42:53 -0700

    setup update

--- setup.sh
+++ setup.sh
@@ -2,14 +2,22 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Setting up ROOT"
-
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-
-alias cmssw1068='cwd=${PWD}; cd /cvmfs/cms.cern.ch/slc7_amd64_gcc700/cms/cmssw/CMSSW_10_6_8/src; cmsenv; cd $cwd; echo Now using CMSSW v10.6.8'
-alias cmssw1134='cwd=${PWD}; cd /cvmfs/cms.cern.ch/slc7_amd64_gcc900/cms/cmssw/CMSSW_11_3_4/src; cmsenv; cd $cwd; echo Now using CMSSW v11.3.4'
-alias cmssw1220='cwd=${PWD}; cd /cvmfs/cms.cern.ch/slc7_amd64_gcc900/cms/cmssw/CMSSW_12_2_0/src; cmsenv; cd $cwd; echo Now using CMSSW v12.2.0'
-alias cmssw1221pre4='cwd=${PWD}; cd /cvmfs/cms.cern.ch/slc7_amd64_gcc900/cms/cmssw/CMSSW_12_1_0_pre4_ROOT624/src; cmsenv; cd $cwd; echo Now using CMSSW v12.2.1 (pre4, ROOT 6.24)'
+alias cmssw1021='export SCRAM_ARCH=slc7_amd64_gcc700; export CMSSW_VERSION=CMSSW_10_2_13'
+alias cmssw1068='export SCRAM_ARCH=slc7_amd64_gcc700; export CMSSW_VERSION=CMSSW_10_6_8'
+alias cmssw1134='export SCRAM_ARCH=slc7_amd64_gcc900; export CMSSW_VERSION=CMSSW_11_3_4'
+alias cmssw1220='export SCRAM_ARCH=slc7_amd64_gcc900; export CMSSW_VERSION=CMSSW_12_2_0'
+alias cmssw1221prev4='export SCRAM_ARCH=slc7_amd64_gcc900; export CMSSW_VERSION=CMSSW_12_1_0_prev4_ROOT624'
 
 cmssw1220
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /cvmfs/cms.cern.ch/$SCRAM_ARCH/cms/cmssw/$CMSSW_VERSION/src
+eval `scramv1 runtime -sh`
+cd - > /dev/null
 
+echo 'Setup following ROOT'
+which root
+
+export LD_LIBRARY_PATH=$DIR:$LD_LIBRARY_PATH
+export PYTHONPATH="$DIR:$PYTHONPATH"
+export PATH=$DIR:$PATH
+export LD_LIBRARY_PATH=$PWD/NanoTools/NanoCORE:$LD_LIBRARY_PATH
