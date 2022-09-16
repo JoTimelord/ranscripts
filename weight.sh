ef84f8a72ecaba5e26c0332cedb94078c72689cf
Yuntong Zhou <joytzphysics@uaf-10.t2.ucsd.edu>
2022-09-16 11:21:46 -0700

    update on weight.sh

--- weight.sh
+++ weight.sh
@@ -26,12 +26,9 @@ VBSWZH_C2V_4 \
 VBSZZH_C2V_4 \
 VBSZZH_C2V_3"
 
-#rm -f weightsbkg.txt
-#rm -f weightssig.txt
-rm -f jobs.txt
-mkdir -p logfiles
-mkdir -p outputs
-
+rm -f .jobs.txt
+rm -f weightssig.txt
+rm -f weightsbkg.txt
 
 
 for BKGPROCESS in ${BKGPROCESSES}; do
@@ -55,26 +52,25 @@ for BKGPROCESS in ${BKGPROCESSES}; do
         XSECTION=72100
         KEY="TTTo2L2Nu_TuneCP5"
     fi
-    echo ${BKGPROCESS}
     IFILE=0
     for YEAR in ${BKGYEARS}; do
         if [[ ${YEAR} == "20UL16NanoAODv9" ]]; then LUMINOSITY=16.81; fi
         if [[ ${YEAR} == "20UL16NanoAODAPVv9" ]]; then LUMINOSITY=19.52; fi
         if [[ ${YEAR} == "20UL17NanoAODv9" ]]; then LUMINOSITY=41.48; fi
         if [[ ${YEAR} == "20UL18NanoAODv9" ]]; then LUMINOSITY=59.83; fi
-        for INPUTDIR in $(ls ${BKGDIR} | grep ${KEY} | grep ${YEAR}); do
+        for INPUTDIR in $(ls ${BKGDIR} | grep ^${KEY} | grep ${YEAR}); do
+            echo ${INPUTDIR}
             IFILE=$(( IFILE+1 ))
             SUMMEDWGT=$( python3 scale1fb.py -f ${BKGDIR}/${INPUTDIR} )
-            SCALE1FB=$( echo "scale=10; ${XSECTION}/${SUMMEDWGT}" | bc )
-            #echo ${BKGPROCESS} ${KEY} ${YEAR} ${LUMINOSITY} ${XSECTION} ${SUMMEDWGT} ${SCALE1FB} >> weightsbkg.txt
-            echo "./runNanoLooper --input ${BKGDIR}/${INPUTDIR}/ --output outputs/${BKGPROCESS}_${IFILE}.root --scale1fb ${SCALE1FB} --lumi ${LUMINOSITY} > logfiles/${BKGPROCESS}_${IFILE}.log 2>&1" >> .jobs.txt
+            SCALE1FB=$( echo "scale=10; ${XSECTION}/${SUMMEDWGT}" | bc | awk '{printf "%f", $0}' )
+            echo ${BKGPROCESS} ${KEY} ${YEAR} ${LUMINOSITY} ${XSECTION} ${SUMMEDWGT} ${SCALE1FB} >> weightsbkg.txt
+            echo "./runNanoLooper --input ${BKGDIR}/${INPUTDIR}/ --output outputs/${BKGPROCESS}_${IFILE}.root --scale1fb ${SCALE1FB} --lumi ${LUMINOSITY} > ~/NanoLooper/logfiles/${BKGPROCESS}_${IFILE}.log 2>&1" >> .jobs.txt
         done
     done
 done
 
 
 for SIGPROCESS in ${SIGPROCESSES}; do
-    echo ${SIGPROCESS}
     if [[ ${SIGPROCESS} == *"VBSOSWWH_C2V_3" ]]
     then
         XSECTION=5.652
@@ -112,11 +108,12 @@ for SIGPROCESS in ${SIGPROCESSES}; do
         if [[ ${YEAR} == "20UL17" ]]; then LUMINOSITY=41.48; fi
         if [[ ${YEAR} == "20UL18" ]]; then LUMINOSITY=59.83; fi
         for INPUTDIR in $(ls ${SIGDIR} | grep ${KEY} | grep ${YEAR}); do
+            echo ${INPUTDIR}
             IFILE=$(( IFILE+1 ))
             SUMMEDWGT=$( python3 scale1fb.py -f ${SIGDIR}/${INPUTDIR} )
-            SCALE1FB=$( echo "scale=10; ${XSECTION}/${SUMMEDWGT}" | bc )
-            #echo ${SIGPROCESS} ${KEY} ${YEAR} ${XSECTION} ${SUMMEDWGT} ${SCALE1FB} >> weightssig.txt
-            echo "./runNanoLooper --input ${SIGDIR}/${INPUTDIR}/ --output outputs/${SIGPROCESS}_${IFILE}.root --scale1fb ${SCALE1FB} --lumi ${LUMINOSITY} > logfiles/${SIGPROCESS}_${IFILE}.log 2>&1" >> .jobs.txt
+            SCALE1FB=$( echo "scale=10; ${XSECTION}/${SUMMEDWGT}" | bc | awk '{printf "%f", $0}' )
+            echo ${SIGPROCESS} ${KEY} ${YEAR} ${XSECTION} ${SUMMEDWGT} ${SCALE1FB} >> weightssig.txt
+            echo "./runNanoLooper --input ${SIGDIR}/${INPUTDIR}/ --output outputs/${SIGPROCESS}_${IFILE}.root --scale1fb ${SCALE1FB} --lumi ${LUMINOSITY} > ~/NanoLooper/logfiles/${SIGPROCESS}_${IFILE}.log 2>&1" >> .jobs.txt
         done
     done
 done
